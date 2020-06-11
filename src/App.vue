629248a65c8f5d1c81bd9ff7a19092fb0049130a
wzb3422 <wzb3422@gmail.com>
2020-06-12 00:11:35 +0800

    :art: feat: qr layout

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,10 @@
     </v-app-bar>
 
     <v-content>
-      <v-card class="d-flex flex-row justify-center" flat tile>
-        <v-card class="d-lg-6 d-sm-12">
+      <v-card class="d-flex flex-row justify-center mt-12" flat tile>
+        <v-card class="d-lg-6 d-sm-12 px-5" raised shaped>
           <v-card-title>Scan QR to Login</v-card-title>
+          <v-img class="mb-4" src="https://i.loli.net/2020/06/11/mejz6dPtTquJWli.png" max-width="200"></v-img>
         </v-card>
       </v-card>
     </v-content>
